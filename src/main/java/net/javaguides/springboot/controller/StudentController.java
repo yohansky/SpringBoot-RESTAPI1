1f7cc82f9fc057cb3289ef8c9d5c6042e9d2fcae
Yohanes <yohanesdahlan@gmail.com>
2024-07-06 14:56:05 +0700

    Student Controller (get students)

--- src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -4,6 +4,9 @@ import net.javaguides.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     // http://localhost:8080/student
@@ -15,5 +18,16 @@ public class StudentController {
                 "Hubert"
         );
         return student;
+    }
+
+    // http://localhost:8080/students
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1,"Yohanes","Hubert"));
+        students.add(new Student(2,"Hubert","Yohanes"));
+        students.add(new Student(3,"Joey","Yohanes"));
+        students.add(new Student(4,"Hubert","Joey"));
+        return students;
     }
 }
