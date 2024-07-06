c6b1560e36aff783c35a01723a78ca01c9cc393d
Yohanes <yohanesdahlan@gmail.com>
2024-07-06 14:40:51 +0700

    Student Controller (get)

---
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -0,0 +1,19 @@
+package net.javaguides.springboot.controller;
+
+import net.javaguides.springboot.bean.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    // http://localhost:8080/student
+    @GetMapping("student")
+    public Student getStudent(){
+        Student student = new Student(
+                1,
+                "Yohanes",
+                "Hubert"
+        );
+        return student;
+    }
+}
